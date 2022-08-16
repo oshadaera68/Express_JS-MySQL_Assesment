248cfde0bace665d638797d280c2d114f3dd8329
oshadaera68 <mindartlk010@gmail.com>
2022-08-16 23:18:11 +0530

    crud no 1 - get all customer task completed

--- Routes/customer.js
+++ Routes/customer.js
@@ -1,3 +1,4 @@
+const { query } = require('express')
 const express = require('express')
 const mysql = require('mysql')
 const dbase = require('../configs/db.configs')
@@ -7,17 +8,24 @@ const connection = mysql.createConnection(dbase.database)
 connection.connect(function (err) {
     if (err) {
         console.log(err);
-    }else{
+    } else {
         console.log("Connection Established and connected mysql");
         var cusTable = "CREATE TABLE IF NOT EXISTS customer (id VARCHAR(255) PRIMARY KEY, name VARCHAR(255), address TEXT, salary DOUBLE)"
-        connection.query(cusTable,function(err,result){
-            if(result.warningCount===0){
+        connection.query(cusTable, function (err, result) {
+            if (result.warningCount === 0) {
                 console.log("table created");
             }
         })
     }
 })
 
+router.get('/', (req, res) => {
+    var getAllQuery = "SELECT * FROM customer";
+    connection.query(getAllQuery, (err, rows) => {
+        if (err) console.log(err);
+        res.send(rows); 
+    })
+})
 
 
 module.exports = router;
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.use(express.json())
 
-app.use('/customers',customer)
+app.use('/customer',customer)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
