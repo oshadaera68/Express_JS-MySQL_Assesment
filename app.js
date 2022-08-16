7ce196a798bdf6a5de61fced2ac126ea7a7dd885
oshadaera68 <mindartlk010@gmail.com>
2022-08-16 21:29:40 +0530

    added customer.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const customer = require('./Routes/customer')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+app.use('/customers',customer)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
