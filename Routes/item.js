3c9dbfeed897a0c7f45f3bee461b937f48a9687d
oshadaera68 <mindartlk010@gmail.com>
2022-08-17 23:00:43 +0530

    crud step 1 - get all items completed

--- Routes/item.js
+++ Routes/item.js
@@ -6,16 +6,25 @@ const connection = mysql.createConnection(dbase.database)
 
 // creating item table
 connection.connect(function (err) {
-    if(err){
+    if (err) {
         console.log(err);
-    }else{
+    } else {
         var itemTable = "CREATE TABLE IF NOT EXISTS item (code VARCHAR(15) PRIMARY KEY, description TEXT, qtyOnHand INT, unitPrice DOUBLE)"
         connection.query(itemTable, function (err, result) {
-            if (result.warningCount===0) {
+            if (result.warningCount === 0) {
                 console.log("table created");
             }
         })
     }
+})
+
+// get all item
+router.get('/', (req, res) => {
+    var getAllItemQuery = "SELECT * FROM item";
+    connection.query(getAllItemQuery, (err, rows) => {
+        if (err) console.log(err);
+        res.send(rows);
+    })
 })
 
 module.exports = router;
